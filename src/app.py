bc238c094f0e395a08028aa8b4db4bacf2787279
imanoop7 <64773373+imanoop7@users.noreply.github.com>
2024-02-04 22:47:47 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,13 +1,16 @@
 import streamlit as st 
+from langchain_core.message import AIMessage, HumanMessage
 
 def get_response(user_input):
     return
 
 
 st.set_page_config(page_title = "Chat with websites", page_icon=" ")
-
-
 st.title("Chat with websites")
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history=[
+        AIMessage(content="Hello, How can I help you ?")
+    ]
 
 with st.sidebar:
     st.header("Settings")
@@ -17,6 +20,8 @@ with st.sidebar:
 user_query = st.chat_input("Type your message here....")
 if user_query is not None and user_query !='':
     response = get_response(user_query)
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(AIMessage(content=response))
     with st.chat_message("HUMAN"):
         st.write(user_query)
 
